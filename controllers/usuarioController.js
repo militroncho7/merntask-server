74afdaec08c1380c079e3f0c3ae885aafa7c8f9b
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-05-28 13:36:10 +0200

    insertando usuarios

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,21 @@
+const Usuario = require('../models/Usuario');
 
-exports.crearUsuario = (req, res) => {
-    console.log(req.body);
+exports.crearUsuario = async (req, res) => {
+    
+    try{
+        let usuario;
+
+        //Crea nuevo usuario
+        usuario = new Usuario(req.body);
+
+        //Guarda usuario
+        await usuario.save();
+
+        //Mensaje de confirmación
+        res.send('Usuario creado correctamente');
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
 };
