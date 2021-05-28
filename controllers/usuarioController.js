ab8c4d6d3dc3766e2ca51445b2c64598cd0e514f
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-05-28 10:12:39 +0200

    controlador para crear usuarios

---
+++ controllers/usuarioController.js
@@ -0,0 +1,4 @@
+
+exports.crearUsuario = (req, res) => {
+    console.log('desde crearUsuario')
+};
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,12 @@
 //Rutas para crear usuarios
 const express = require('express');
 const router = express.Router();
+const usuarioController = require('../controllers/usuarioController');
 
 //Crea un usuario
 //endpoitn: api/usuarios
-router.post('/', () => {}); 
+router.post('/',
+    usuarioController.crearUsuario
+); 
 
 module.exports = router;
