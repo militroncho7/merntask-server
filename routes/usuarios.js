2e355cae73e3418496ec44db05b891dbe447b39c
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-05-28 14:02:24 +0200

    validacion API: express-validator

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,10 +2,16 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
+const { check } = require('express-validator');
 
 //Crea un usuario
 //endpoitn: api/usuarios
 router.post('/',
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'Agrega un email valido').isEmail(),
+        check('password', 'El password debe ser mínimo de 6 caracteres').isLength({ min: 6 })
+    ],
     usuarioController.crearUsuario
 ); 
 
