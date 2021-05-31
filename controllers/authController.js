edf004d168e0b5290086fd46343388a384e46244
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-05-31 11:20:15 +0200

    check: usuario y password correcto-incorrecto (token)

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,5 +10,42 @@ exports.autenticarUsuario = async (req, res) => {
         return res.status(400).json({ errores: errores.array() })
     };
 
-    
+    //Extraer email y password del request
+    const { email, password } = req.body;
+
+    try {
+        //Revisar que sea un usuario registrado
+        let usuario = await Usuario.findOne({ email });
+
+        if(!usuario) {
+            return res.status(400).json({ msg: 'El usuario no existe '});
+        }
+
+        //revisar password
+        const passCorrecto = await bcryptjs.compare(password, usuario.password); //compara password
+        if(!passCorrecto) {
+            return res.status(400).json({ msg: 'Password Incorrecto' });
+        }
+
+        //Si todo es correcto: Crear y firmar el jwt (token)
+        const payload = {
+            usuario: {
+                id: usuario.id
+            }
+        };
+
+        //Firmar token
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 //1hora de caducidad del token
+        }, (error, token) => {
+            if(error) throw error;
+
+            //Mensaje de confirmación
+            res.json({ token } );
+        });
+
+    } catch (error) {
+        console.log(error);        
+    }
+
 };
