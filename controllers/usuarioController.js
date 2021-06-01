cef5f5903d398c26df01c8b18a3e6c4a26507533
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-06-01 10:00:07 +0200

    validacion proyectos

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -9,7 +9,7 @@ exports.crearUsuario = async (req, res) => {
     const errores = validationResult(req);
     if( !errores.isEmpty() ) {
         return res.status(400).json({ errores: errores.array() })
-    }
+    };
 
     //Extraer email y password
     const { email, password } = req.body;
